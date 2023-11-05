1d24c0dd18aab618d9e4b0af08076346358b1883
Mikko Rautiainen <m_rautiainen@hotmail.com>
2023-11-05 10:09:37 +0200

    unitigify

--- src/repeatgraph.cpp
+++ src/repeatgraph.cpp
@@ -63,6 +63,38 @@ void writeGraph(std::string outputFileName, const std::vector<size_t>& nodeCover
 	std::cerr << countEdges << " graph edges" << std::endl;
 }
 
+void writeGraph(std::string outputFileName, const std::vector<double>& nodeCoverage, const std::vector<size_t>& nodeLength, const MostlySparse2DHashmap<uint8_t, size_t>& edgeCoverage, const size_t minCoverage, const size_t k)
+{
+	std::ofstream graph { outputFileName };
+	size_t countNodes = 0;
+	size_t countEdges = 0;
+	for (size_t i = 0; i < nodeCoverage.size(); i++)
+	{
+		if (nodeCoverage[i] < minCoverage) continue;
+		graph << "S\tnode_" << i << "\t*\tLN:i:" << (nodeLength[i]+k-1) << "\tll:f:" << nodeCoverage[i] << "\tFC:i:" << nodeCoverage[i] * (nodeLength[i]+k-1) << std::endl;
+		countNodes += 1;
+	}
+	for (size_t i = 0; i < edgeCoverage.size(); i++)
+	{
+		auto fw = std::make_pair(i, true);
+		for (auto pair : edgeCoverage.getValues(fw))
+		{
+			if (pair.second < minCoverage) continue;
+			graph << "L\tnode_" << i << "\t+\tnode_" << pair.first.first << "\t" << (pair.first.second ? "+" : "-") << "\t" << (k-1) << "M\tec:i:" << pair.second << std::endl;
+			countEdges += 1;
+		}
+		auto bw = std::make_pair(i, false);
+		for (auto pair : edgeCoverage.getValues(bw))
+		{
+			if (pair.second < minCoverage) continue;
+			graph << "L\tnode_" << i << "\t-\tnode_" << pair.first.first << "\t" << (pair.first.second ? "+" : "-") << "\t" << (k-1) << "M\tec:i:" << pair.second << std::endl;
+			countEdges += 1;
+		}
+	}
+	std::cerr << countNodes << " graph nodes" << std::endl;
+	std::cerr << countEdges << " graph edges" << std::endl;
+}
+
 uint64_t find(std::vector<uint64_t>& result, const size_t pos)
 {
 	assert((pos & firstBitUint64_t) == 0);
@@ -601,6 +633,218 @@ std::vector<size_t> getNodeLengths(const std::vector<uint64_t>& segments, const 
 	return result;
 }
 
+void startUnitig(const size_t startPos, const std::vector<uint64_t>& uniqueLeftEdge, const std::vector<uint64_t>& uniqueRightEdge, std::vector<bool>& belongsToUnitig, std::vector<std::vector<uint64_t>>& unitigs, std::vector<uint64_t>& unitigLeftmostNode, std::vector<uint64_t>& unitigRightmostNode)
+{
+	assert(!belongsToUnitig[startPos & maskUint64_t]);
+	uint64_t unitigStartPos = startPos;
+	while (true)
+	{
+		uint64_t next;
+		if (unitigStartPos & firstBitUint64_t)
+		{
+			next = uniqueLeftEdge[unitigStartPos & maskUint64_t];
+		}
+		else
+		{
+			next = uniqueRightEdge[unitigStartPos & maskUint64_t];
+		}
+		if (next >= std::numeric_limits<size_t>::max()-1) break;
+		next ^= firstBitUint64_t;
+		if ((next & maskUint64_t) == (unitigStartPos & maskUint64_t)) break;
+		if (next & firstBitUint64_t)
+		{
+			if (uniqueRightEdge[next & maskUint64_t] >= std::numeric_limits<size_t>::max()-1) break;
+			assert(uniqueRightEdge[next & maskUint64_t] == unitigStartPos);
+		}
+		else
+		{
+			if (uniqueLeftEdge[next & maskUint64_t] >= std::numeric_limits<size_t>::max()-1) break;
+			assert(uniqueLeftEdge[next & maskUint64_t] == unitigStartPos);
+		}
+		if (next == startPos) break;
+		assert(!belongsToUnitig[next & maskUint64_t]);
+		unitigStartPos = next;
+	}
+	unitigs.emplace_back();
+	unitigs.back().push_back(unitigStartPos);
+	unitigLeftmostNode.emplace_back(unitigStartPos ^ firstBitUint64_t);
+	uint64_t pos = unitigStartPos;
+	belongsToUnitig[pos & maskUint64_t] = true;
+	while (true)
+	{
+		uint64_t next;
+		if (pos & firstBitUint64_t)
+		{
+			next = uniqueRightEdge[pos & maskUint64_t];
+		}
+		else
+		{
+			next = uniqueLeftEdge[pos & maskUint64_t];
+		}
+		if (next >= std::numeric_limits<size_t>::max()-1) break;
+		if ((next & maskUint64_t) == (pos & maskUint64_t)) break;
+		if (next & firstBitUint64_t)
+		{
+			if (uniqueLeftEdge[next & maskUint64_t] >= std::numeric_limits<size_t>::max()-1) break;
+			assert(uniqueLeftEdge[next & maskUint64_t] == (pos ^ firstBitUint64_t));
+		}
+		else
+		{
+			if (uniqueRightEdge[next & maskUint64_t] >= std::numeric_limits<size_t>::max()-1) break;
+			assert(uniqueRightEdge[next & maskUint64_t] == (pos ^ firstBitUint64_t));
+		}
+		if (next == unitigStartPos) break;
+		pos = next;
+		assert(!belongsToUnitig[pos & maskUint64_t]);
+		belongsToUnitig[pos & maskUint64_t] = true;
+		unitigs.back().push_back(pos);
+	}
+	unitigRightmostNode.emplace_back(pos);
+}
+
+std::tuple<std::vector<std::vector<uint64_t>>, std::vector<uint64_t>, std::vector<uint64_t>> getUnitigs(const size_t countNodes, const size_t minCoverage, const MostlySparse2DHashmap<uint8_t, size_t>& edgeCoverages)
+{
+	std::vector<uint64_t> uniqueRightEdge;
+	std::vector<uint64_t> uniqueLeftEdge;
+	uniqueRightEdge.resize(countNodes, std::numeric_limits<size_t>::max());
+	uniqueLeftEdge.resize(countNodes, std::numeric_limits<size_t>::max());
+	for (size_t i = 0; i < countNodes; i++)
+	{
+		for (auto pair : edgeCoverages.getValues(std::make_pair(i, true)))
+		{
+			if (pair.second < minCoverage) continue;
+			if (uniqueRightEdge[i] == std::numeric_limits<size_t>::max())
+			{
+				uniqueRightEdge[i] = pair.first.first + (pair.first.second ? firstBitUint64_t : 0);
+			}
+			else
+			{
+				uniqueRightEdge[i] = std::numeric_limits<size_t>::max()-1;
+			}
+			if (pair.first.second)
+			{
+				if (uniqueLeftEdge[pair.first.first] == std::numeric_limits<size_t>::max())
+				{
+					uniqueLeftEdge[pair.first.first] = i;
+				}
+				else
+				{
+					uniqueLeftEdge[pair.first.first] = std::numeric_limits<size_t>::max()-1;
+				}
+			}
+			else
+			{
+				if (uniqueRightEdge[pair.first.first] == std::numeric_limits<size_t>::max())
+				{
+					uniqueRightEdge[pair.first.first] = i;
+				}
+				else
+				{
+					uniqueRightEdge[pair.first.first] = std::numeric_limits<size_t>::max()-1;
+				}
+			}
+		}
+		for (auto pair : edgeCoverages.getValues(std::make_pair(i, false)))
+		{
+			if (pair.second < minCoverage) continue;
+			if (uniqueLeftEdge[i] == std::numeric_limits<size_t>::max())
+			{
+				uniqueLeftEdge[i] = pair.first.first + (pair.first.second ? firstBitUint64_t : 0);
+			}
+			else
+			{
+				uniqueLeftEdge[i] = std::numeric_limits<size_t>::max()-1;
+			}
+			if (pair.first.second)
+			{
+				if (uniqueLeftEdge[pair.first.first] == std::numeric_limits<size_t>::max())
+				{
+					uniqueLeftEdge[pair.first.first] = i + firstBitUint64_t;
+				}
+				else
+				{
+					uniqueLeftEdge[pair.first.first] = std::numeric_limits<size_t>::max()-1;
+				}
+			}
+			else
+			{
+				if (uniqueRightEdge[pair.first.first] == std::numeric_limits<size_t>::max())
+				{
+					uniqueRightEdge[pair.first.first] = i + firstBitUint64_t;
+				}
+				else
+				{
+					uniqueRightEdge[pair.first.first] = std::numeric_limits<size_t>::max()-1;
+				}
+			}
+		}
+	}
+	std::vector<bool> belongsToUnitig;
+	belongsToUnitig.resize(countNodes, false);
+	std::vector<std::vector<uint64_t>> unitigs;
+	std::vector<uint64_t> unitigLeftmostNode;
+	std::vector<uint64_t> unitigRightmostNode;
+	for (size_t i = 0; i < countNodes; i++)
+	{
+		if (belongsToUnitig[i]) continue;
+		startUnitig(i, uniqueLeftEdge, uniqueRightEdge, belongsToUnitig, unitigs, unitigLeftmostNode, unitigRightmostNode);
+	}
+	return std::make_tuple(std::move(unitigs), std::move(unitigLeftmostNode), std::move(unitigRightmostNode));
+}
+
+std::pair<std::vector<size_t>, std::vector<double>> getUnitigLengthAndCoverage(const std::vector<std::vector<uint64_t>>& unitigs, const std::vector<size_t>& nodeCoverage, const std::vector<size_t>& nodeLength)
+{
+	std::vector<double> coverage;
+	std::vector<size_t> length;
+	coverage.resize(unitigs.size(), 0);
+	length.resize(unitigs.size(), 0);
+	for (size_t i = 0; i < unitigs.size(); i++)
+	{
+		for (uint64_t node : unitigs[i])
+		{
+			coverage[i] += nodeLength[node & maskUint64_t] * nodeCoverage[node & maskUint64_t];
+			length[i] += nodeLength[node & maskUint64_t];
+		}
+		coverage[i] /= (double)length[i];
+	}
+	return std::make_pair(std::move(length), std::move(coverage));
+}
+
+MostlySparse2DHashmap<uint8_t, size_t> getUnitigEdgeCoverages(const std::vector<uint64_t>& unitigLeftmostNode, const std::vector<uint64_t>& unitigRightmostNode, const size_t minCoverage, const MostlySparse2DHashmap<uint8_t, size_t>& edgeCoverages)
+{
+	phmap::flat_hash_map<uint64_t, uint64_t> nodeToUnitig;
+	assert(unitigLeftmostNode.size() == unitigRightmostNode.size());
+	MostlySparse2DHashmap<uint8_t, size_t> result;
+	result.resize(unitigLeftmostNode.size());
+	for (size_t i = 0; i < unitigLeftmostNode.size(); i++)
+	{
+		assert(nodeToUnitig.count(unitigLeftmostNode[i] ^ firstBitUint64_t) == 0);
+		nodeToUnitig[unitigLeftmostNode[i] ^ firstBitUint64_t] = i + firstBitUint64_t;
+		assert(nodeToUnitig.count(unitigRightmostNode[i] ^ firstBitUint64_t) == 0);
+		nodeToUnitig[unitigRightmostNode[i] ^ firstBitUint64_t] = i;
+	}
+	for (size_t i = 0; i < unitigLeftmostNode.size(); i++)
+	{
+		for (auto pair : edgeCoverages.getValues(std::make_pair(unitigLeftmostNode[i] & maskUint64_t, (unitigLeftmostNode[i] & firstBitUint64_t) == firstBitUint64_t)))
+		{
+			if (pair.second < minCoverage) continue;
+			uint64_t kmerKey = pair.first.first + (pair.first.second ? firstBitUint64_t : 0);
+			assert(nodeToUnitig.count(kmerKey) == 1);
+			uint64_t targetUnitig = nodeToUnitig.at(kmerKey);
+			result.set(std::make_pair(i, false), std::make_pair(targetUnitig & maskUint64_t, (targetUnitig & firstBitUint64_t) == firstBitUint64_t), pair.second);
+		}
+		for (auto pair : edgeCoverages.getValues(std::make_pair(unitigRightmostNode[i] & maskUint64_t, (unitigRightmostNode[i] & firstBitUint64_t) == firstBitUint64_t)))
+		{
+			if (pair.second < minCoverage) continue;
+			uint64_t kmerKey = pair.first.first + (pair.first.second ? firstBitUint64_t : 0);
+			assert(nodeToUnitig.count(kmerKey) == 1);
+			uint64_t targetUnitig = nodeToUnitig.at(kmerKey);
+			result.set(std::make_pair(i, true), std::make_pair(targetUnitig & maskUint64_t, (targetUnitig & firstBitUint64_t) == firstBitUint64_t), pair.second);
+		}
+	}
+	return result;
+}
+
 void makeGraph(const std::vector<size_t>& readLengths, const std::vector<MatchGroup>& matches, const size_t minCoverage, const std::string& outputFileName, const size_t k)
 {
 	std::vector<RankBitvector> breakpoints = extendBreakpoints(readLengths, matches);
@@ -622,7 +866,16 @@ void makeGraph(const std::vector<size_t>& readLengths, const std::vector<MatchGr
 	std::vector<size_t> nodeLength = getNodeLengths(segments, segmentToNode, breakpoints, countNodes);
 	MostlySparse2DHashmap<uint8_t, size_t> edgeCoverages = getEdgeCoverages(readLengths, segmentToNode, segments, breakpoints, minCoverage, nodeCoverage, countNodes);
 	std::cerr << edgeCoverages.size() << " edges pre coverage filter" << std::endl;
-	writeGraph(outputFileName, nodeCoverage, nodeLength, edgeCoverages, minCoverage, k);
+	std::vector<std::vector<uint64_t>> unitigs;
+	std::vector<uint64_t> unitigLeftmostNode;
+	std::vector<uint64_t> unitigRightmostNode;
+	std::tie(unitigs, unitigLeftmostNode, unitigRightmostNode) = getUnitigs(countNodes, minCoverage, edgeCoverages);
+	std::vector<size_t> unitigLength;
+	std::vector<double> unitigCoverage;
+	std::tie(unitigLength, unitigCoverage) = getUnitigLengthAndCoverage(unitigs, nodeCoverage, nodeLength);
+	MostlySparse2DHashmap<uint8_t, size_t> unitigEdgeCoverages = getUnitigEdgeCoverages(unitigLeftmostNode, unitigRightmostNode, minCoverage, edgeCoverages);
+	// writeGraph(outputFileName, nodeCoverage, nodeLength, edgeCoverages, minCoverage, k);
+	writeGraph(outputFileName, unitigCoverage, unitigLength, unitigEdgeCoverages, minCoverage, k);
 }
 
 template <typename F>
